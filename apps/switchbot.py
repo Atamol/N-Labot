cd605d3e8bc62cf4c3a0db7b7a346f1c18a578c0
Atamol <35617831+Atamol@users.noreply.github.com>
2025-02-09 14:39:25 +0900

    Update switchbot.py

--- apps/switchbot.py
+++ apps/switchbot.py
@@ -6,12 +6,15 @@ import hmac
 import hashlib
 import requests
 
+import discord
+from discord import app_commands
+
 SWITCHBOT_TOKEN = os.getenv("SWITCHBOT_TOKEN")
 SWITCHBOT_SECRET = os.getenv("SWITCHBOT_SECRET")
 SWITCHBOT_DEVICE_ID = os.getenv("SWITCHBOT_DEVICE_ID")
 
 # 署名付きヘッダを作成
-def make_auth_headers(token: str, secret: str):
+def make_auth_headers(token: str, secret: str) -> dict:
     t = str(int(time.time() * 1000))
     nonce = uuid.uuid4().hex
     string_to_sign = token + t + nonce
@@ -22,7 +25,7 @@ def make_auth_headers(token: str, secret: str):
             hashlib.sha256
         ).digest()
     ).decode("utf-8")
-
+    
     return {
         "Authorization": f"Bearer {token}",
         "sign": sign,
@@ -31,19 +34,23 @@ def make_auth_headers(token: str, secret: str):
         "Content-Type": "application/json"
     }
 
-# ステータスの取得
-def get_meter_status():
+# ステータス情報を取得
+def get_meter_status() -> dict:
     if not (SWITCHBOT_TOKEN and SWITCHBOT_SECRET and SWITCHBOT_DEVICE_ID):
         print("SwitchBot関連の環境変数が設定されていません。")
         return {}
-
+    
     headers = make_auth_headers(SWITCHBOT_TOKEN, SWITCHBOT_SECRET)
     url = f"https://api.switch-bot.com/v1.1/devices/{SWITCHBOT_DEVICE_ID}/status"
-    res = requests.get(url, headers=headers)
-    data = res.json()
-
+    try:
+        res = requests.get(url, headers=headers)
+        data = res.json()
+    except Exception as e:
+        print("SwitchBot API request error:", e)
+        return {}
+    
     if data.get("statusCode") == 100:
-        return data["body"]
+        return data.get("body", {})
     else:
         print("SwitchBot API Error:", data)
         return {}
